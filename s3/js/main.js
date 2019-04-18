ca20375563763e94cab61d906b1a52e75331ed2f
alfax@fedora29 <jorge.riverac82@gmail.com>
2019-04-18 09:35:08 -0500

    Add new function getProfiles()

--- s3/js/main.js
+++ s3/js/main.js
@@ -131,13 +131,19 @@ function getFormData() {
     purposesReference.push(projectObject)
 }
 
-$("#submit-button").on("click", function() {
+$("#submit-button").on("click", function () {
     getFormData()
 })
 
-$("#get-purposes").on("click",function(){
-	getPurposes();
+$("#get-purposes").on("click", function () {
+    getPurposes();
 })
+
+// $("#profiles-wrapper").on("click", function () {
+//     getProfiles();
+// })
+
+// $("#profiles-wrapper").getProfiles();
 
 var projectArray = [];
 
@@ -156,15 +162,15 @@ var projectPurposeHtml = `<div class="col-lg-3">
 								</div>
 							</div>`
 
+
 function getPurposes() {
-    
     console.log(purposesContent)
     $("#purposes-wrapper").empty();
     /*Object.values(purposesContent).forEach(function(value){
         console.log(value);
         projectArray.push(value);
     });*/
-    $.each(purposesContent,function(key,value){
+    $.each(purposesContent, function (key, value) {
         console.log(value);
         var projectName = value.projectName;
         var projectDescription = value.projectDescription;
@@ -191,10 +197,11 @@ function getPurposes() {
 
 
 
-purposesReference.on('value', function(snapshot) {
+purposesReference.on('value', function (snapshot) {
     console.log(snapshot.val());
     purposesContent = snapshot.val();
     getPurposes();
+    getProfiles();
 });
 
 /*var newPurposal = {
@@ -205,4 +212,224 @@ purposesReference.on('value', function(snapshot) {
     customerName: "new"
 }*/
 
+/**
+ * Esta variable guarda la porción de código de cada perfil mostrado en index.html
+ */
+var projectProfilesHtml = `<div class="col-12 col-lg-4">
+					<div class="card border-0">
+						<img src="img/profile-pic.jpg" class="card-img-top rounded-circle w-50 mx-auto mt-3 mt-lg-4"
+							alt="...">
+						<div class="card-body">
+							<h2 class="text-info text-center">Israel Salinas Martinez</h2>
+							<p class="card-text font-weight-bold font-italic text-center">Lead teacher @Kodemia</p>
+						</div>
+						<div class="data-wrapper mb-3 w-100">
+							<h4 class="data-title bg-info p-2 text-uppercase">contacto</h4>
+							<p class="text-uppercase font-weight-bold mt-3">55 43 15 78 96 <br> israel@kodemia.mx
+								<br>www.kodemia.mx</p>
+						</div>
+						<div class="data-wrapper mb-3 w-100">
+							<h4 class="data-title bg-info p-2 text-uppercase">social media</h4>
+							<p class="text-uppercase font-weight-bold mt-3">facebook /razieliquibalam<br> twitter
+								@razieliquibalam <br>instagram @razieliquibalam</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-lg-8 bg-info ">
+					<div class="data-wrapper mb-3 mx-auto mt-3 mt-lg-4">
+						<h4 class="data-title bg-info p-2 text-uppercase bg-white ">perfil</h4>
+						<p class="text-uppercase font-weight-bold mt-3 text-justify">Lorem ipsum dolor sit amet,
+							consectetur adipisicing elit. Possimus, facere? Lorem ipsum dolor sit amet, consectetur
+							adipisicing elit. Ipsa est, fuga illo porro dolore amet?</p>
+					</div>
+					<div class="data-wrapper mb-3 mx-auto">
+						<h4 class="data-title bg-info p-2 text-uppercase bg-white ">Experiencia</h4>
+						<div class="description-wrapper">
+							<p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+							<p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+							<ul>
+								<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+									velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+								<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+									velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+							</ul>
+						</div>
+						<div class="description-wrapper">
+							<p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+							<p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+							<ul>
+								<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+									velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+								<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+									velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+							</ul>
+						</div>
+					</div>
+					<div class="data-wrapper mb-3 mx-auto">
+						<h4 class="data-title bg-info p-2 text-uppercase bg-white ">Educación</h4>
+						<div class="description-wrapper">
+							<p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+							<p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+						</div>
+						<div class="description-wrapper">
+							<p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+							<p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+						</div>
+					</div>
+					<div class="data-wrapper mb-3 mx-auto">
+						<h4 class="data-title bg-info p-2 text-uppercase bg-white ">Habilidades</h4>
+						<div class="row">
+							<div class="col-12 col-lg-6">
+								<ul class="mb-0">
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+								</ul>
+							</div>
+							<div class="col-12 col-lg-6">
+								<ul class="mb-0">
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+									<li>Lorem ipsum dolor sit amet.</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="data-wrapper mb-3 mx-auto">
+						<h4 class="data-title bg-info p-2 text-uppercase bg-white ">Reconocimientos</h4>
+						<div class="description-wrapper">
+							<p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+							<p class="text-uppercase small">Lorem ipsum dolor sit amet. <br>Month Day Year</p>
+						</div>
+						<div class="description-wrapper">
+							<p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+							<p class="text-uppercase small">Lorem ipsum dolor sit amet. <br>Month Day Year</p>
+						</div>
+					</div>
+				</div>`
 
+
+/**
+ * Esta función inserta un perfil de freelancer en index.html, por cada proposal en la base de datos
+ */
+function getProfiles() {
+    console.log(purposesContent)
+    $("#profiles-wrapper").empty();
+    /*Object.values(purposesContent).forEach(function(value){
+        console.log(value);
+        projectArray.push(value);
+    });*/
+    $.each(purposesContent, function (key, value) {
+        console.log(value);
+        var projectName = value.projectName;
+        var projectDescription = value.projectDescription;
+        var customerName = value.customerName;
+        var customerPhone = value.customerPhone;
+        var customerMail = value.customerMail;
+
+        var projectProfilesHtml = `<div class="col-12 col-lg-4">
+        <div class="card border-0">
+            <img src="img/profile-pic.jpg" class="card-img-top rounded-circle w-50 mx-auto mt-3 mt-lg-4"
+                alt="...">
+            <div class="card-body">
+        var customerName = value.customerName;
+                <h2 class="text-info text-center">${customerName}</h2>
+                <p class="card-text font-weight-bold font-italic text-center">${projectName}</p>
+            </div>
+            <div class="data-wrapper mb-3 w-100">
+                <h4 class="data-title bg-info p-2 text-uppercase">contacto</h4>
+                <p class="text-uppercase font-weight-bold mt-3">${customerPhone} <br> ${customerMail}
+                    <br>www.kodemia.mx</p>
+            </div>
+            <div class="data-wrapper mb-3 w-100">
+                <h4 class="data-title bg-info p-2 text-uppercase">social media</h4>
+                <p class="text-uppercase font-weight-bold mt-3">facebook /razieliquibalam<br> twitter
+                    @razieliquibalam <br>instagram @razieliquibalam</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-lg-8 bg-info ">
+        <div class="data-wrapper mb-3 mx-auto mt-3 mt-lg-4">
+            <h4 class="data-title bg-info p-2 text-uppercase bg-white ">perfil</h4>
+            <p class="text-uppercase font-weight-bold mt-3 text-justify">Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Possimus, facere? Lorem ipsum dolor sit amet, consectetur
+                adipisicing elit. Ipsa est, fuga illo porro dolore amet?</p>
+        </div>
+        <div class="data-wrapper mb-3 mx-auto">
+            <h4 class="data-title bg-info p-2 text-uppercase bg-white ">Experiencia</h4>
+            <div class="description-wrapper">
+                <p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+                <p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+                        velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+                        velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+                </ul>
+            </div>
+            <div class="description-wrapper">
+                <p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+                <p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+                        velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+                    <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque id laudantium
+                        velit reprehenderit rerum, magnam iusto assumenda illum commodi eligendi.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="data-wrapper mb-3 mx-auto">
+            <h4 class="data-title bg-info p-2 text-uppercase bg-white ">Educación</h4>
+            <div class="description-wrapper">
+                <p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+                <p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+            </div>
+            <div class="description-wrapper">
+                <p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+                <p class="text-uppercase small">Lorem ipsum dolor sit amet. | Month Day Year</p>
+            </div>
+        </div>
+        <div class="data-wrapper mb-3 mx-auto">
+            <h4 class="data-title bg-info p-2 text-uppercase bg-white ">Habilidades</h4>
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <ul class="mb-0">
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <ul class="mb-0">
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="data-wrapper mb-3 mx-auto">
+            <h4 class="data-title bg-info p-2 text-uppercase bg-white ">Reconocimientos</h4>
+            <div class="description-wrapper">
+                <p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+                <p class="text-uppercase small">Lorem ipsum dolor sit amet. <br>Month Day Year</p>
+            </div>
+            <div class="description-wrapper">
+                <p class="lead font-weight-bold font-italic mb-0">Lorem ipsum dolor sit amet.</p>
+                <p class="text-uppercase small">Lorem ipsum dolor sit amet. <br>Month Day Year</p>
+            </div>
+        </div>
+    </div>
+    <div class="w-100">&nbsp;</div>`
+
+        $("#profiles-wrapper").append(projectProfilesHtml);
+    })
+}
